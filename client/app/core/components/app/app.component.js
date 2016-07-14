1e674d86d6b17581d869a55229c5da7eb653ca74
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2016-07-14 10:44:12 +0200

    How to use an example vendor module on a component

--- client/app/core/components/app/app.component.js
+++ client/app/core/components/app/app.component.js
@@ -3,6 +3,7 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import template from './app.template.html';
 import { MenuComponent } from '../menu/menu.component';
+import { var1, var2 } from '../../../../vendor_modules/test.js';
 
 @Component({
   selector: 'my-app',
@@ -18,5 +19,7 @@ export class AppComponent {
 
   ngOnInit() {
     console.log('App onInit');
+    console.log(var1);
+    console.log(var2);
   }
 }
